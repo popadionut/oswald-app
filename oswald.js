5a52219c69ffe38c35f4fe6fdc365cc66d3a2d2e
ASUS-LAPTOP\Ionut Popa <popadionut@gmail.com>
2016-03-29 19:14:39 +0300

    pl

---
+++ oswald.js
@@ -0,0 +1,74 @@
+Tasks = new Mongo.Collection("tasks");
+ 
+if (Meteor.isClient) {
+    Meteor.subscribe("tasks");
+  // This code only runs on the client
+  Template.body.helpers({
+      tasks: function () {
+           if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+         },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
+      // Show newest tasks at the top
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+    
+    Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var text = event.target.text.value;
+ 
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+       createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  // username of logged in user
+      });
+ 
+      // Clear form
+         event.target.text.value = "";
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
+    }
+  });
+             Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
+    Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+}
+if (Meteor.isServer) {
+  // This code only runs on the server
+  // Only publish tasks that are public or belong to the current user
+  Meteor.publish("tasks", function () {
+    return Tasks.find({
+      $or: [
+        { owner: this.userId }
+      ]
+    });
+  });
+}
